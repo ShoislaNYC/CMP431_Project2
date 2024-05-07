6f1f09fb32256bc259f0e49ad4d28307b8ac577c
Shohag <shoislamNYC@gmail.com>
2024-05-07 00:12:17 -0400

    first commit

---
+++ app/src/main/java/com/example/knownyc/presentation/navigation/AppNavigation.kt
@@ -0,0 +1,23 @@
+package com.example.knownyc.presentation.navigation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigationGraph(){
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.HOME_SCREEN ) {
+        composable(Routes.HOME_SCREEN){
+            Text(text = "Hello")
+        }
+        composable(Routes.PARKS_SCREEN){
+            
+        }
+        
+    }
+
+}
---
+++ app/src/main/java/com/example/knownyc/presentation/navigation/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.knownyc.presentation.navigation
+
+object Routes {
+    const val HOME_SCREEN = "HOME"
+    const val PARKS_SCREEN = "PARKS"
+}
