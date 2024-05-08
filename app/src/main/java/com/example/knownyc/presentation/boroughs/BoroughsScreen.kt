b4fca652283364c5806cf3c96abbedfed5d1029e
Shohag <shoislamNYC@gmail.com>
2024-05-08 18:12:14 -0400

    Created UI state for boroughs and added some usable commons code

--- app/src/main/java/com/example/knownyc/presentation/boroughs/BoroughsScreen.kt
+++ app/src/main/java/com/example/knownyc/presentation/boroughs/BoroughsScreen.kt
@@ -1,12 +1,31 @@
 package com.example.knownyc.presentation.boroughs
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.knownyc.domain.models.Borough
 
 @Composable
 fun BoroughsScreen(
     modifier: Modifier = Modifier,
 ){
-    
+    val viewModel: BoroughsViewModel = hiltViewModel()
+    val state by viewModel.state.collectAsStateWithLifecycle()
 
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(state.boroughs){borough ->
+            BoroughCard(name = borough.name,
+                painter = painterResource(id = borough.image),
+                contentDescription = borough.longName
+            )
+        }
+    }
 }
